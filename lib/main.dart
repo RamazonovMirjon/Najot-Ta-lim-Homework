7ba7b4d7eac1c97dc059d8d00c0e09f6947ac8fd
Mirjon Ramazonov <mirxan2564@gmail.com>
2022-02-08 15:59:52 +0500

    kichik uzgarish

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ramazonov',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
